241b73d97464800828f79cd8660d0fee16ae982e
aggresss <aggresss@163.com>
2018-04-19 22:10:38 +0800

    20180419A

--- xrr.sh
+++ xrr.sh
@@ -5,12 +5,12 @@ elif [ $1 = 'v1' ]; then
 	xrandr --output VGA1 --left-of LVDS1 --auto
 elif [ $1 = 'v2' ]; then
 	xrandr --output VGA1 --right-of LVDS1 --auto
-elif [ $1 = 'h0' ]; then
-	xrandr --output HDMI1 --off
-elif [ $1 = 'h1' ]; then
-	xrandr --output HDMI1 --left-of LVDS1 --auto
-elif [ $1 = 'h2' ]; then
-	xrandr --output HDMI1 --right-of LVDS1 --auto
+elif [ $1 = 'd0' ]; then
+	xrandr --output DP1 --off
+elif [ $1 = 'd1' ]; then
+	xrandr --output DP1 --left-of LVDS1 --auto
+elif [ $1 = 'd2' ]; then
+	xrandr --output DP1 --right-of LVDS1 --auto
 else
 	echo -e "\e[31m please input args  v0,v1,v2  or  h0,h1,h2. \e[0m"
 fi
