6a49810d6f1ff6d21d26f8d3e8102ff0bf283ff3
aggresss <aggresss@163.com>
2018-07-02 14:36:03 +0800

    2018072C

--- keeplid.sh
+++ keeplid.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # toggle close the lid operation on and off
-# initial run: sudo echo "#HandleLidSwitch=ignore" >> /etc/systemd/logind.conf
 
 if [ $1 = 0 ];then
     # execute disable
-    sudo sed -i 's/HandleLidSwitch=ignore/#HandleLidSwitch=ignore/' /etc/systemd/logind.conf
+    sudo sed -i 's/^HandleLidSwitch=ignore/HandleLidSwitch=suspend/' /etc/systemd/logind.conf
+    sudo sed -i 's/^HandleLidSwitchDocked=ignore/HandleLidSwitchDocked=suspend/' /etc/systemd/logind.conf
     sudo systemctl restart systemd-logind
     ps -ef|grep 32221:127.0.0.1 |grep -v grep|awk '{print $2}'|xargs kill -9
     echo -e "\e[31m Close the lattop lid will suspend. \e[0m"
@@ -15,7 +15,10 @@ if [ $1 = 1 ];then
     # execute enable
     ps -ef|grep 32221:127.0.0.1 |grep -v grep|awk '{print $2}'|xargs kill -9
     sleep 1
-    sudo sed -i 's/#HandleLidSwitch=ignore/HandleLidSwitch=ignore/' /etc/systemd/logind.conf
+    sudo sed -i 's/^#HandleLidSwitch=suspend/HandleLidSwitch=ignore/' /etc/systemd/logind.conf
+    sudo sed -i 's/^#HandleLidSwitchDocked=ignore/HandleLidSwitchDocked=ignore/' /etc/systemd/logind.conf
+    sudo sed -i 's/^HandleLidSwitch=suspend/HandleLidSwitch=ignore/' /etc/systemd/logind.conf
+    sudo sed -i 's/^HandleLidSwitchDocked=suspend/HandleLidSwitchDocked=ignore/' /etc/systemd/logind.conf
     sudo systemctl restart systemd-logind
     autossh -M 32221 -fnN -R 32220:127.0.0.1:22 agg.vultr
     echo -e "\e[32m Close the lattop lid will keep on working. \e[0m"
