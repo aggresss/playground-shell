46a8ffafa851ab39fdd6af3d8bb95d058f863925
aggresss <aggresss@163.com>
2018-07-04 14:48:37 +0800

    20180704B

--- set-gopath.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# set $PWD append to $GOPATH
-if [[ $GOPATH =~ .*$PWD.* ]]
-then
-    echo "currnet dir is already in GOPATH"
-else
-    export GOPATH=$GOPATH:$PWD
-    echo "fininsh setting $PWD in GOPATH"
-fi
