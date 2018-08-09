6ce7b4a9498d82a089506d923de7b1ad21b2b2bc
aggresss <aggresss@gmail.com>
2018-08-09 11:42:31 +0800

    20180809A

--- inittab.sh
+++ inittab.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # Change default runlevel on Debian systemd based system
 
+# linux shell color support.
+RED="\\033[31m"
+GREEN="\\033[32m"
+YELLOW="\\033[33m"
+BLACK="\\033[0m"
+
 if [ "X_$1" = "X_" ]; then
     runlevel
 elif [ $1 -ge 0 -a $1 -le 6 ]; then
     sudo ln -sf /lib/systemd/system/runlevel$1.target /etc/systemd/system/default.target
 else
-    echo -e "\e[31m please input args 0-6 \e[0m"
+    echo -e "${RED} please input args 0-6 ${BLACK}"
 fi
 #end
