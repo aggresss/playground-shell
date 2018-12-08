b59b58392afde3c66a517396978fcfdb0f70d82f
aggresss <aggresss@163.com>
2018-12-08 14:10:24 +0800

    2018-12-08 14:10:24 CST W49D6

--- go_install.sh
+++ go_install.sh
@@ -7,9 +7,16 @@ BASE_URL_1="http://repo.router7.com/go"
 BASE_URL_2="https://dl.google.com/go"
 
 if [ ${1-NoDefine} = "NoDefine" ]; then
-    GO_VERSION="1.10.5"
+    GO_VERSION="go1.10.5"
 else
-    GO_VERSION="$1"
+    GO_VERSION="go$1"
+fi
+
+CUR_VERSION=$(go version | awk '{print $3}')
+
+if [ ${GO_VERSION} = ${CUR_VERSION} ]; then
+    echo "Current Go version is already update."
+    exit 0
 fi
 
 ###  function for download and extract to assign path ####
@@ -27,9 +34,13 @@ function down_load
         echo "Download $1 failed."
         return 2
     fi
-    if [ ! -d $2 ]; then
-        mkdir -p $2
+
+    # !!! ONLY use in go_install
+    if [ -d $2 ]; then
+        rm -rf $2
     fi
+    mkdir -p $2
+
     if [ $file_ext = "gz" -o $file_ext = "bz2" ]; then
         tar -vxf ${down_file} -C $2 --strip-components 1
         rm -rf ${down_file}
@@ -39,12 +50,12 @@ function down_load
 if [ $(uname -m) = "x86_64" ]; then
     case $(uname) in
     Darwin)
-        down_load ${BASE_URL_1}/go${GO_VERSION}.darwin-amd64.tar.gz ${HOME}/.local/go \
-        || down_load ${BASE_URL_2}/go${GO_VERSION}.darwin-amd64.tar.gz ${HOME}/.local/go
+        down_load ${BASE_URL_1}/${GO_VERSION}.darwin-amd64.tar.gz ${HOME}/.local/go \
+        || down_load ${BASE_URL_2}/${GO_VERSION}.darwin-amd64.tar.gz ${HOME}/.local/go
     ;;
     Linux)
-        down_load ${BASE_URL_1}/go${GO_VERSION}.linux-amd64.tar.gz ${HOME}/.local/go \
-        || down_load ${BASE_URL_2}/go${GO_VERSION}.linux-amd64.tar.gz ${HOME}/.local/go
+        down_load ${BASE_URL_1}/${GO_VERSION}.linux-amd64.tar.gz ${HOME}/.local/go \
+        || down_load ${BASE_URL_2}/${GO_VERSION}.linux-amd64.tar.gz ${HOME}/.local/go
     ;;
     *)
         echo "Operating system not support."
