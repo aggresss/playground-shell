2cbe8f01820ae3208160238d9f24e63b72b9f95f
aggresss <aggresss@163.com>
2018-12-27 15:14:56 +0800

    2018-12-27 15:14:56 CST W52D4

--- switch_mirror.sh
+++ switch_mirror.sh
@@ -12,19 +12,19 @@ WHITE="\\033[37m"
 NORMAL="\\033[m"
 
 if [[ $(uname) = "Linux" ]]; then
-    if [ -f /etc/apt/source.list.origin ]; then
-        sudo mv -f /etc/apt/sources.list.origin /etc/apt/source.list
+    if [ -f /etc/apt/sources.list.origin ]; then
+        sudo mv -f /etc/apt/sources.list.origin /etc/apt/sources.list
         echo -e "${YELLOW}Reset mirror file success${NORMAL}"
     else
         DISTRIBUTION=$(cat /etc/issue | awk '{print $1}')
         case ${DISTRIBUTION} in
             Ubuntu)
-                sudo cp -f /etc/apt/sources.list /etc/apt/source.list.origin
+                sudo cp -f /etc/apt/sources.list /etc/apt/sources.list.origin
                 sudo sed -i 's/archive.ubuntu/mirrors.aliyun/g' /etc/apt/sources.list
                 echo -e "${GREEN}Alternate ubuntu mirror to aliyun${NORMAL}"
             ;;
             Debian)
-                sudo cp -f /etc/apt/sources.list /etc/apt/source.list.origin
+                sudo cp -f /etc/apt/sources.list /etc/apt/sources.list.origin
                 sudo sed -i 's/deb.debian.org/mirrors.aliyun.com/g' /etc/apt/sources.list
                 echo -e "${GREEN}Alternate debian mirror to aliyun${NORMAL}"
             ;;
