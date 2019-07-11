6b69eac7ff1b1906bf08bab29f72435ac4e20f74
aggresss <aggresss@163.com>
2019-07-11 14:00:56 +0800

    2019-07-11 14:00:56 CST W27D4

---
+++ docker_install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# docker
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-using-the-repository
+sudo apt-get remove -y docker docker-engine docker.io && \
+    sudo apt-get update && \
+    sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+sudo apt-get update && sudo apt-get install -y docker-ce
+# add docker access authority to system default user
+sudo usermod -a `id -nu 1000` -G docker
+
