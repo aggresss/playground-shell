ebbaf148df5566ac7907d5388dd876af7a32235e
aggresss <aggresss@163.com>
2019-07-21 11:14:21 +0800

    2019-07-21 11:14:21 CST W28D7

--- hello.sh
+++ hello.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # hello test for various kinds of language
 
 # linux shell color support.
---
+++ wallpaper_install.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# $1 download url
+# $2 local filepath
+function update_file()
+{
+    local tmp_path="/tmp"
+    # can replace by dirname and basename command
+    local down_file=`echo "$1" | awk -F "/" '{print $NF}'`
+    local down_path=`echo "$2" | awk 'BEGIN{res=""; FS="/";}{for(i=2;i<=NF-1;i++) res=(res"/"$i);} END{print res}'`
+    if [ ! -d ${down_path} ]; then
+        mkdir -vp ${down_path}
+    fi
+    rm -rvf ${tmp_path}/${down_file}
+    if [ $(command -v wget > /dev/null; echo $?) -eq 0 ]; then
+        wget -P ${tmp_path} $1
+    elif [ $(command -v curl > /dev/null; echo $?) -eq 0 ]; then
+        cd ${tmp_path}
+        curl -OL $1
+        cd -
+    else
+        echo "No http request tool."
+        exit 1;
+    fi
+    cp -vf ${tmp_path}/${down_file} $2
+    rm -vf ${tmp_path}/${down_file}
+    if [ ${down_file##*.} = "sh" ]; then
+        chmod +x $2
+    fi
+}
+
+WALLPAPER_DIR="${HOME}/Pictures/Wallpaper"
+mkdir -p ${WALLPAPER_DIR}
+update_file http://repo.router7.com/pictures/winter_fog.jpg ${WALLPAPER_DIR}/default.jpg
