ad803a12f8746e94a8664ae63dc8a4a238b4a796
aggresss <aggresss@163.com>
2019-10-10 16:11:04 +0800

    2019-10-10 16:11:04 CST W40D4

--- hello.sh
+++ hello.sh
@@ -187,6 +187,21 @@ install(TARGETS main RUNTIME DESTINATION bin)
 END
         echo "/tmp/CMakeLists.txt"
     ;;
+    nodejs)
+        cat << END > /tmp/hello.js
+#!/usr/bin/env node
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello, World!\n');
+}).listen(8080, "0.0.0.0");
+console.log('Server running at http://:localhost:8080/');
+
+END
+    echo "/tmp/hello.js"
+    chmod +x /tmp/hello.js
+    ;;
     *)
         echo -e "${GREEN}Support Lang:"
         echo -e "  c\n  cpp\n  go\n  rs\n  py\n  sh\n  pl\n${NORMAL}"
