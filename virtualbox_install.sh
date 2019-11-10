84c1e8b0af802db24eee221450fb77cd131052c5
aggresss <aggresss@163.com>
2019-11-10 20:31:26 +0800

    2019-11-10 20:31:26 CST W44D7

--- virtualbox_install.sh
+++ virtualbox_install.sh
@@ -8,5 +8,5 @@ sudo add-apt-repository \
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install virtualbox-6.0
+sudo apt-get install virtualbox-6.0 virtualbox-ext-pack
 
