a2edc75c07b73043af1b182c3441a565a360a998
aggresss <aggresss@163.com>
2020-01-27 19:40:42 +0800

    2020-01-27 19:40:42 CST W04D1

--- hello.sh
+++ hello.sh
@@ -217,16 +217,16 @@ END
         cat << END > /tmp/hello.html
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="UTF-8">
-  <title>Hello, World!</title>
-</head>
-<body>
-  <h1>Hello, World!</h1>
-  <script>
-    console.log('Hello, World!')
-  </script>
-</body>
+  <head>
+    <meta charset="UTF-8">
+    <title>Hello, World!</title>
+  </head>
+  <body>
+    <h1>Hello, World!</h1>
+    <script>
+      console.log('Hello, World!')
+    </script>
+  </body>
 </html>
 
 END
