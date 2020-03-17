a4c7785a38d401f93027b53edd5d39956799e849
aggresss <aggresss@163.com>
2020-03-17 09:12:07 +0800

    2020-03-17 09:12:07 CST W11D2

---
+++ workbench/trap_test.sh
@@ -0,0 +1,16 @@
+#/usr/bin/env bash
+
+# test trap command
+trap "echo 'Sorry! I have trapped Ctrl-C'" SIGINT
+
+echo This is a test script
+
+count=1
+while [ $count -le 10 ]
+do
+  echo "Loop $count"
+  sleep 1
+  count=$[ $count + 1 ]
+done
+
+echo The end.
