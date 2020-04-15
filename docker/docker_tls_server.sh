db4d5ef587c5a6c6b6d1a2c400da2e8946c7b87a
aggresss <aggresss@163.com>
2020-04-15 21:18:09 +0800

    2020-04-15 21:18:09 CST W15D3

--- docker/docker_tls_server.sh
+++ docker/docker_tls_server.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cd certs
+mkdir -p /etc/docker/ssl
 sudo cp -v ca.crt /etc/docker/ssl/ca.pem
 sudo cp -v server.key /etc/docker/ssl/key.pem
 sudo cp -v server.crt /etc/docker/ssl/cert.pem
