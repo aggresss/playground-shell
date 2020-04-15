69811af440ae4858f6174c46355962d1fdbfd5d7
aggresss <aggresss@163.com>
2020-04-15 20:52:51 +0800

    2020-04-15 20:52:51 CST W15D3

--- docker/docker_tls_client.sh
+++ docker/docker_tls_client.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
+
+read -p "Please input server hostname:" input_host
+if [ ${input_host:-NOCONFIG} = "NOCONFIG" ]; then
+    cert_path="${HOME}/.docker"
+else
+    echo ${input_host}
+    cert_path="${HOME}/.docker/${input_host}"
+fi
+
 cd certs
-mkdir -p ${HOME}/.docker
-cp -v ca.crt ${HOME}/.docker/ca.pem
-cp -v client.key ${HOME}/.docker/key.pem
-cp -v client.crt ${HOME}/.docker/cert.pem
+mkdir -p ${cert_path}
+cp -v ca.crt ${cert_path}/ca.pem
+cp -v client.key ${cert_path}/key.pem
+cp -v client.crt ${cert_path}/cert.pem
+
