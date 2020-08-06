b3312ad40606215442fbc021e57e10c19cb53595
aggresss <aggresss@163.com>
2020-08-06 09:16:42 +0800

    2020-08-06 09:16:42 CST W31D4

--- utilities/webp2png.sh
+++ utilities/webp2png.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # webp 格式文件转换
+set -e
 
 for file in `ls *.webp`
-    do  ffmpeg -i $file `echo ${file%.*}.png`
+    do  ffmpeg -i $file `echo ${file%.*}.png` && rm -vf $file
 done
+
