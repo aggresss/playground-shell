0dc48e555cc84a9dabbaee3cad9fb2e6007e3176
aggresss <aggresss@163.com>
2021-02-06 09:43:42 +0800

    2021-02-06 09:43:42 CST W05D6

---
+++ sh/utilities/detect_shell.sh
@@ -0,0 +1,14 @@
+
+
+function detect_shell_type() {
+    echo ${SHELL}
+}
+
+function detect_array_start_index() {
+    local x=(1 0)
+    echo "Array start index:" ${x[1]}
+}
+
+detect_shell_type
+detect_array_start_index
+
