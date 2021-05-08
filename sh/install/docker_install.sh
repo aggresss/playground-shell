8662d834f3e4256deea3b0056abecc2339b43359
aggresss <aggresss@163.com>
2021-05-08 13:15:25 +0800

    2021-05-08 13:15:25 CST W18D6

--- sh/install/docker_install.sh
+++ sh/install/docker_install.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 # docker
-# https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-using-the-repository
-sudo apt-get remove -y docker docker-engine docker.io && \
+# https://docs.docker.com/engine/install/ubuntu/
+sudo apt-get remove docker docker-engine docker.io containerd runc && \
     sudo apt-get update && \
     sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
-sudo apt-get update && sudo apt-get install -y docker-ce
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 # add docker access authority to system default user
 sudo usermod -a `id -nu 1000` -G docker
-
--- sh/install/virtualbox_install.sh
+++ sh/install/virtualbox_install.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 # virtualbox
 # https://www.virtualbox.org/wiki/Linux_Downloads
-sudo add-apt-repository \
-    "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian \
-    $(lsb_release -cs) \
-    contrib"
+
+echo \
+  "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian \
+  $(lsb_release -cs) \
+  contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list > /dev/null
+
+
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install virtualbox-6.0
+sudo apt-get install virtualbox-6.1
 sudo usermod -aG vboxusers $USER
-
