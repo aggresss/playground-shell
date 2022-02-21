77534d77db9be755581c8b1ef177396bafd33083
aggresss <aggresss@163.com>
2022-02-21 14:24:27 +0800

    2022-02-21 14:24:11 CST W08D1

---
+++ sh/utilities/rfc_join.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# https://www.baeldung.com/linux/join-multiple-lines
+
+if [ ${1:-NOCONFIG} = "NOCONFIG" ]; then
+    echo 'please input filename.'
+    exit 1
+fi
+
+gsed -i ':a; N; $!ba; s/   //g; s/\n\n/\v/g; s/\n/ /g; s/\v/\n/g' $1
