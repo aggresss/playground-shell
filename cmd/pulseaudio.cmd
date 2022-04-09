59468b46ad3846fca724a72e3d667f2d186f69a0
aggresss <aggresss@163.com>
2022-04-09 21:17:17 +0800

    2022-04-09 21:17:17 +08 W14D6

--- cmd/pulseaudio.cmd
+++ cmd/pulseaudio.cmd
@@ -1,4 +1,4 @@
-REM launch PulseAudio with arguments
+@REM launch PulseAudio with arguments
 @echo off
 setlocal
 %USERPROFILE%\bin\pulseaudio-1.1\bin\pulseaudio.exe --exit-idle-time=-1 --load="module-native-protocol-tcp auth-ip-acl=0.0.0.0/0" --load="module-esound-protocol-tcp auth-ip-acl=0.0.0.0/0"
