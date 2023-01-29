7bfa2a28935062bcf58c718092e802a7988637e4
aggresss <aggresss@163.com>
2023-01-29 11:25:33 +0800

    2023-01-29 11:25:33 CST W05D0

--- sh/utilities/rfc_join.sh
+++ sh/utilities/rfc_join.sh
@@ -6,5 +6,12 @@ if [ ${1:-NOCONFIG} = "NOCONFIG" ]; then
     exit 1
 fi
 
-gsed -i ':a; N; $!ba; s/   //g; s/\n\n/\v/g; s/\n/ /g; s/\v/\n/g' $1
-gsed -i '/\[Page/d' $1
+SED_CMD="sed"
+case $(uname) in
+Darwin)
+    SED_CMD="gsed"
+    ;;
+esac
+
+${SED_CMD} -i ':a; N; $!ba; s/   //g; s/\n\n/\v/g; s/\n/ /g; s/\v/\n/g' $1
+${SED_CMD} -i '/\[Page/d' $1
