5d99991607044198a92a0e61ce8222e9d723314d
aggresss <aggresss@163.com>
2024-07-09 17:33:17 +0800

    2024-07-09 17:33:17 CST W27D2

--- sh/workbench/linux_tc_base.sh
+++ sh/workbench/linux_tc_base.sh
@@ -1,20 +1,34 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
+#
+# enp1s0 -> external
+# enp2s0 -> internal
+#
+
+export INF_NAME="enp2s0"
+
+tc qdisc del dev ${INF_NAME} handle ffff: ingress
+tc qdisc del dev ifb0 root
+tc qdisc del dev ${INF_NAME} root
 
 set -e
 
-INTERFACE_NAME="enx000ec62565b5"
-
-tc filter del dev ifb0 root
-tc qdisc del dev ifb0 root
-tc qdisc del dev ${INTERFACE_NAME} handle ffff: ingress
-
-modprobe ifb numifbs=1
+ip link add dev ifb0 type ifb
 ip link set dev ifb0 up
-tc qdisc add dev ${INTERFACE_NAME} handle ffff: ingress
-tc filter add dev ${INTERFACE_NAME} parent ffff: protocol ip u32 match u32 0 0 action mirred egress redirect dev ifb0
-tc qdisc add dev ifb0 root handle 1:0 htb
-tc class add dev ifb0 parent 1: classid 1:1 htb rate 500mbit
-tc class add dev ifb0 parent 1:1 classid 1:10 htb rate 50mbit
-tc qdisc add dev ${INTERFACE_NAME} root handle 1:0 htb
-tc class add dev ${INTERFACE_NAME} parent 1: classid 1:1 htb rate 500mbit
-tc class add dev ${INTERFACE_NAME} parent 1:1 classid 1:10 htb rate 50mbit
+tc qdisc add dev ${INF_NAME} handle ffff: ingress
+tc filter add dev ${INF_NAME} parent ffff: protocol ip u32 match u32 0 0 action mirred egress redirect dev ifb0
+
+tc qdisc add dev ifb0 root handle 1: htb default 1
+tc class add dev ifb0 parent 1: classid 1:1 htb rate 1Gbit quantum 10000
+
+tc qdisc add dev ${INF_NAME} root handle 1: htb default 1
+tc class add dev ${INF_NAME} parent 1: classid 1:1 htb rate 1Gbit quantum 10000
+
+########
+
+tc class add dev ifb0 parent 1:1 classid 1:10 htb rate 10mbit
+tc qdisc add dev ifb0 parent 1:10 handle 10: sfq perturb 10
+tc filter add dev ifb0 protocol ip parent 1: prio 1 u32 match ip dport 22 0xffff flowid 1:10
+
+tc class add dev ${INTERFACE_NAME} parent 1:1 classid 1:10 htb rate 10mbit
+tc qdisc add dev ${INTERFACE_NAME} parent 1:10 handle 10: sfq perturb 10
+tc filter add dev ${INTERFACE_NAME} protocol ip parent 1: prio 1 u32 match ip sport 22 0xffff flowid 1:10
