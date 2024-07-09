cdbcc499232e687c4748f558db8e8e001a069a02
aggresss <aggresss@163.com>
2024-07-09 17:45:12 +0800

    2024-07-09 17:45:12 CST W27D2

--- sh/workbench/linux_tc_base.sh
+++ sh/workbench/linux_tc_base.sh
@@ -10,10 +10,11 @@ tc qdisc del dev ${INF_NAME} handle ffff: ingress
 tc qdisc del dev ifb0 root
 tc qdisc del dev ${INF_NAME} root
 
+ip link add dev ifb0 type ifb >/dev/null 2>&1
+ip link set dev ifb0 up
+
 set -e
 
-ip link add dev ifb0 type ifb
-ip link set dev ifb0 up
 tc qdisc add dev ${INF_NAME} handle ffff: ingress
 tc filter add dev ${INF_NAME} parent ffff: protocol ip u32 match u32 0 0 action mirred egress redirect dev ifb0
 
@@ -29,6 +30,8 @@ tc class add dev ifb0 parent 1:1 classid 1:10 htb rate 10mbit
 tc qdisc add dev ifb0 parent 1:10 handle 10: sfq perturb 10
 tc filter add dev ifb0 protocol ip parent 1: prio 1 u32 match ip dport 22 0xffff flowid 1:10
 
-tc class add dev ${INTERFACE_NAME} parent 1:1 classid 1:10 htb rate 10mbit
-tc qdisc add dev ${INTERFACE_NAME} parent 1:10 handle 10: sfq perturb 10
-tc filter add dev ${INTERFACE_NAME} protocol ip parent 1: prio 1 u32 match ip sport 22 0xffff flowid 1:10
+tc class add dev ${INF_NAME} parent 1:1 classid 1:10 htb rate 10mbit
+tc qdisc add dev ${INF_NAME} parent 1:10 handle 10: sfq perturb 10
+tc filter add dev ${INF_NAME} protocol ip parent 1: prio 1 u32 match ip sport 22 0xffff flowid 1:10
+
+echo "tc base successful."
--- sh/workbench/linux_tc_config.sh
+++ sh/workbench/linux_tc_config.sh
@@ -10,6 +10,6 @@ tc class add dev ifb0 parent 1:1 classid 1:20 htb rate 10mbit
 tc qdisc add dev ifb0 parent 1:20 handle 20: netem delay 500ms loss 30%
 tc filter add dev ifb0 protocol ip parent 1: prio 3 u32 match ip src 192.168.200.101 flowid 1:20
 
-tc class add dev ${INTERFACE_NAME} parent 1:1 classid 1:20 htb rate 10mbit
-tc qdisc add dev ${INTERFACE_NAME} parent 1:20 handle 20: netem delay 500ms loss 30%
-tc filter add dev ${INTERFACE_NAME} protocol ip parent 1: prio 3 u32 match ip dst 192.168.200.101 flowid 1:20
+tc class add dev ${INF_NAME} parent 1:1 classid 1:20 htb rate 10mbit
+tc qdisc add dev ${INF_NAME} parent 1:20 handle 20: netem delay 500ms loss 30%
+tc filter add dev ${INF_NAME} protocol ip parent 1: prio 3 u32 match ip dst 192.168.200.101 flowid 1:20
