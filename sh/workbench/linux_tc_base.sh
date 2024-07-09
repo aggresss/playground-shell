efa79aa16a4af7d9aecbb2b33693b7a45d68347d
aggresss <aggresss@163.com>
2024-07-09 23:28:56 +0800

    2024-07-09 23:28:56 CST W27D2

--- sh/workbench/linux_tc_base.sh
+++ sh/workbench/linux_tc_base.sh
@@ -4,34 +4,35 @@
 # enp2s0 -> internal
 #
 
-export INF_NAME="enp2s0"
+export UP_LINK="ifb0"
+export DOWN_LINK="enp2s0"
 
-tc qdisc del dev ${INF_NAME} handle ffff: ingress
-tc qdisc del dev ifb0 root
-tc qdisc del dev ${INF_NAME} root
+tc qdisc del dev ${DOWN_LINK} handle ffff: ingress
+tc qdisc del dev ${UP_LINK} root
+tc qdisc del dev ${DOWN_LINK} root
 
-ip link add dev ifb0 type ifb >/dev/null 2>&1
-ip link set dev ifb0 up
+ip link add dev ${UP_LINK} type ifb >/dev/null 2>&1
+ip link set dev ${UP_LINK} up
 
 set -e
 
-tc qdisc add dev ${INF_NAME} handle ffff: ingress
-tc filter add dev ${INF_NAME} parent ffff: protocol ip u32 match u32 0 0 action mirred egress redirect dev ifb0
+tc qdisc add dev ${DOWN_LINK} handle ffff: ingress
+tc filter add dev ${DOWN_LINK} parent ffff: protocol ip u32 match u32 0 0 action mirred egress redirect dev ${UP_LINK}
 
-tc qdisc add dev ifb0 root handle 1: htb default 1
-tc class add dev ifb0 parent 1: classid 1:1 htb rate 1Gbit quantum 10000
+tc qdisc add dev ${UP_LINK} root handle 1: htb default 1
+tc class add dev ${UP_LINK} parent 1: classid 1:1 htb rate 1Gbit quantum 10000
 
-tc qdisc add dev ${INF_NAME} root handle 1: htb default 1
-tc class add dev ${INF_NAME} parent 1: classid 1:1 htb rate 1Gbit quantum 10000
+tc qdisc add dev ${DOWN_LINK} root handle 1: htb default 1
+tc class add dev ${DOWN_LINK} parent 1: classid 1:1 htb rate 1Gbit quantum 10000
 
 ########
 
-tc class add dev ifb0 parent 1:1 classid 1:10 htb rate 10mbit
-tc qdisc add dev ifb0 parent 1:10 handle 10: sfq perturb 10
-tc filter add dev ifb0 protocol ip parent 1: prio 1 u32 match ip dport 22 0xffff flowid 1:10
+tc class add dev ${UP_LINK} parent 1:1 classid 1:10 htb rate 10mbit
+tc qdisc add dev ${UP_LINK} parent 1:10 handle 10: sfq perturb 10
+tc filter add dev ${UP_LINK} protocol ip parent 1: prio 1 u32 match ip dport 22 0xffff flowid 1:10
 
-tc class add dev ${INF_NAME} parent 1:1 classid 1:10 htb rate 10mbit
-tc qdisc add dev ${INF_NAME} parent 1:10 handle 10: sfq perturb 10
-tc filter add dev ${INF_NAME} protocol ip parent 1: prio 1 u32 match ip sport 22 0xffff flowid 1:10
+tc class add dev ${DOWN_LINK} parent 1:1 classid 1:10 htb rate 10mbit
+tc qdisc add dev ${DOWN_LINK} parent 1:10 handle 10: sfq perturb 10
+tc filter add dev ${DOWN_LINK} protocol ip parent 1: prio 1 u32 match ip sport 22 0xffff flowid 1:10
 
 echo "tc base successful."
--- sh/workbench/linux_tc_config.sh
+++ sh/workbench/linux_tc_config.sh
@@ -4,12 +4,13 @@
 # enp2s0 -> internal
 #
 
-export INF_NAME="enp2s0"
+export UP_LINK="ifb0"
+export DOWN_LINK="enp2s0"
 
-tc class add dev ifb0 parent 1:1 classid 1:20 htb rate 10mbit
-tc qdisc add dev ifb0 parent 1:20 handle 20: netem delay 500ms loss 30%
-tc filter add dev ifb0 protocol ip parent 1: prio 3 u32 match ip src 192.168.200.101 flowid 1:20
+tc class add dev ${UP_LINK} parent 1:1 classid 1:20 htb rate 10mbit
+tc qdisc add dev ${UP_LINK} parent 1:20 handle 20: netem delay 500ms loss 30%
+tc filter add dev ${UP_LINK} protocol ip parent 1: prio 3 u32 match ip src 192.168.200.101 flowid 1:20
 
-tc class add dev ${INF_NAME} parent 1:1 classid 1:20 htb rate 10mbit
-tc qdisc add dev ${INF_NAME} parent 1:20 handle 20: netem delay 500ms loss 30%
-tc filter add dev ${INF_NAME} protocol ip parent 1: prio 3 u32 match ip dst 192.168.200.101 flowid 1:20
+tc class add dev ${DOWN_LINK} parent 1:1 classid 1:20 htb rate 10mbit
+tc qdisc add dev ${DOWN_LINK} parent 1:20 handle 20: netem delay 500ms loss 30%
+tc filter add dev ${DOWN_LINK} protocol ip parent 1: prio 3 u32 match ip dst 192.168.200.101 flowid 1:20
